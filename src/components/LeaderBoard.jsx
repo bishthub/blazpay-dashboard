7e956bfdec10985ca86f02a0232e4dcda2358db3
anmolbisht2308 <anmolbisht7@gmail.com>
2023-10-03 20:15:09 +0530

    leaderboard points

--- src/components/LeaderBoard.jsx
+++ src/components/LeaderBoard.jsx
@@ -1,95 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import { Link, useLocation } from "react-router-dom";
-// import LeaderData from "../LeaderBoard.json";
-// import axios from "axios";
-// import { toast } from "react-toastify";
-
-// const LeaderBoard = ({ expand }) => {
-//   const location = useLocation();
-
-//   const [inner, setInner] = useState(false);
-
-//   const token = localStorage.getItem("token");
-
-//   const [leaderboardData, setLeaderboardData] = useState([]);
-
-//   async function FetchLeaderData() {
-//     try {
-//       const headers = {
-//         Authorization: `Bearer ${token}`,
-//         "Content-Type": "application/json",
-//       };
-
-//       const response = await axios.get(
-//         "http://localhost:3000/api/user/leaderboard",
-//         { headers: headers }
-//       );
-
-//       if (response.status == 200) {
-//         setLeaderboardData(...response.data);
-//       } else {
-//         toast.warning("Session Timeout");
-//       }
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   }
-
-//   useEffect(() => {
-//     FetchLeaderData();
-//   }, []);
-//   useEffect(() => {
-//     console.log(leaderboardData);
-//   }, []);
-
-//   const sortedData = [...leaderboardData].sort((a, b) => a.rank - b.rank);
-
-//   const leadersToShow =
-//     location.pathname === "/leaderboard" ? sortedData : sortedData.slice(0, 4);
-
-//   useEffect(() => {
-//     if (location.pathname === "/leaderboard") {
-//       setInner(true);
-//     }
-//   }, []);
-
-//   return (
-//     <div
-//       className={`flex flex-col ${expand ? "w-4" : "w-full"} ${
-//         inner ? "h-screen" : "h-full"
-//       } ${
-//         inner ? "overflow-y-scroll " : "overflow-none"
-//       }  gap-5 p-2 bg-gray-800`}
-//     >
-//       <h1 style={{ fontSize: "2rem" }}>Leader Board</h1>
-//       <div className="flex flex-col items-center justify-center w-full p-2">
-//         <div className="flex flex-row items-center justify-around w-full gap-2">
-//           <div className="w-full text-center">#</div>
-//           <div className="w-full text-center">Users</div>
-//           <div className="w-full text-center">Points</div>
-//         </div>
-//         <div className="w-full m-2 border-b border-orange-500 border-solid"></div>
-//         <div className="flex flex-col items-center justify-center w-full gap-6">
-//           {leadersToShow.map((data, idx) => {
-//             return (
-//               <div
-//                 key={idx}
-//                 className="flex flex-row items-center justify-around w-full"
-//               >
-//                 <div className="w-full text-center">{data.rank}</div>
-//                 <div className="w-full text-center">{data.name}</div>
-//                 <div className="w-full text-center">{data.points}</div>
-//               </div>
-//             );
-//           })}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default LeaderBoard;
-
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
@@ -169,7 +77,7 @@ const LeaderBoard = ({ expand }) => {
                 <div className="w-full text-center">{idx + 1}</div>
                 <div className="w-full text-center">{data.username}</div>
                 <div className="w-full text-center">
-                  {data.tokens ? data.tokens : 0}
+                  {data.totalTokens ? data.totalTokens : 0}
                 </div>
               </div>
             );
--- src/pages/Send.jsx
+++ src/pages/Send.jsx
@@ -1,54 +1,112 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import bgImage from "../assets/dashboard_bg.png";
 import SendImg from "../assets/send.png";
 import Button from "../components/Button";
 import { Link } from "react-router-dom";
-
+import TokenInput from "../components/TokenInput";
+import axios from "axios";
 const Send = () => {
+  const [walletAddress, setWalletAddress] = useState([]);
+  const token = localStorage.getItem("token");
+  const WalletChains = useCallback(async () => {
+    try {
+      const headers = {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      };
+      const ChainData = await axios.get("http://localhost:3000/api/wallet", {
+        headers: headers,
+      });
+      if (ChainData.status === 200) {
+        setWalletAddress(ChainData.data);
+        console.log(walletAddress);
+      } else {
+        console.log("Error in getting the chain data in profile page");
+      }
+    } catch (err) {
+      setLoading(false);
+      console.log("Session Expired get wallet address in profile Page");
+    }
+  }, [token]);
+
+  useEffect(() => {
+    WalletChains();
+  }, [WalletChains]);
+
+  useEffect(() => {
+    console.log(walletAddress);
+  }, [walletAddress]);
   return (
-    // <div
-    //   style={{
-    //     backgroundImage: `url(${bgImage})`,
-    //     backgroundSize: "cover",
-    //     backgroundPosition: "center",
-    //     color: "white",
-    //     height: "100vh",
-    //   }}
-    //   className="relative flex justify-center w-full "
-    // >
     <div
       className="relative flex flex-col items-center justify-center w-full h-full gap-3 m-auto bg-black border-2 border-t-4 border-l-4 border-r-4 border-orange-700"
       style={{ width: "25rem", height: "20rem" }}
     >
-      <div className="absolute top-0 flex flex-row items-center justify-center w-full gap-1 p-2 text-black bg-white rounded-lg">
-        <img className="w-8" src={SendImg} alt=" " />
-        <h1 className="text-black text-bold">Send</h1>
-      </div>
-
-      <div className="flex flex-col items-center justify-center w-full gap-3 pl-5 pr-5">
-        <h1 className="mr-auto" style={{ fontSize: "1.3rem" }}>
-          Recipient Address
-        </h1>
-        <div className="w-full h-10 p-2 border-2 border-orange-700 rounded-sm">
-          <input
-            type="text"
-            className="w-full text-center bg-transparent border-none focus:outline-none"
-            placeholder="Enter Address or BlazPay ID"
-          />
+      <form className="w-full">
+        <div className="absolute top-0 flex flex-row items-center justify-center w-full gap-1 p-2 text-black bg-white rounded-lg">
+          <img className="w-8" src={SendImg} alt=" " />
+          <h1 className="text-black text-bold">Send</h1>
         </div>
-        <div className="flex flex-row items-center justify-between w-full gap-3 text-gray-800">
+
+        <div className="flex flex-col items-center justify-center w-full gap-3 pl-5 pr-5">
+          <h1 className="mr-auto" style={{ fontSize: "1.3rem" }}>
+            Recipient Address
+          </h1>
+          <div className="w-full h-10 p-2 border-2 border-orange-700 rounded-sm">
+            {" "}
+            <select
+              // value={selectedWallet}
+              // onChange={handleSelectChange}
+              className="w-full text-center bg-transparent border-none focus:outline-none"
+            >
+              <option value="">Select Wallet Address</option>
+              {walletAddress.chains.map((val, idx) => (
+                <option key={idx} value={val.chainName}>
+                  {val.chainName}
+                </option>
+              ))}
+            </select>
+            <input
+              type="text"
+              className="w-full text-center bg-transparent border-none focus:outline-none"
+              placeholder="Enter Address or BlazPay ID"
+            />
+          </div>
+          {/* <div className="flex flex-row items-center justify-between w-full gap-3 text-gray-800">
           <h1 className="text-gray-500">Network fees</h1>
           <h1 className="text-gray-500">0.005 ETH</h1>
-          <div className="px-4 text-black bg-orange-700 rounded-lg">Fast</div>
+        </div> */}
+          {/* <TokenInput /> */}
+          <div className="flex flex-row w-full h-10 p-2 border border-orange-700 rounded-lg">
+            <div className="flex flex-row items-center justify-around w-3/4 border-r-2 border-orange-700 m-r-2">
+              {/* <h1>img</h1> */}
+              <h1 className="pr-3">ETH</h1>
+              <div className="flex items-center justify-center h-5 p-2 text-xs border border-orange-700 rounded-lg">
+                Etherium
+              </div>
+            </div>
+            <div className="w-1/4">
+              {/* <input
+                type="text"
+                className="w-full bg-transparent border-none focus:outline-none"
+              /> */}
+
+              <input
+                type="number"
+                name="tokens"
+                className="w-full bg-transparent border-none focus:outline-none"
+                value={formData.tokens}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="flex items-center justify-center w-full pl-5 pr-5 mt-3">
-        <Link to="/" className="w-full">
-          <Button name={"Transfer"} />
-        </Link>
-      </div>
+        <div className="flex items-center justify-center w-full pl-5 pr-5 mt-3">
+          <Link to="/" className="w-full">
+            <Button name={"Transfer"} />
+          </Link>
+        </div>
+      </form>
     </div>
-    // </div>
   );
 };
 
